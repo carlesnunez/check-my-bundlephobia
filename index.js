0f1a813564d07c4cfaf28c234456fa95ab4045c3
Carles Núñez Tomeo <carlesnunezt@gmail.com>
2020-03-06 00:48:39 +0100

    Add force data

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit();
 
-github.pullRequests.get({
-    owner: "octokit",
-    repo: "node-github",
-    number: 600,
+octokit.pullRequests.get({
+    owner: "carlesnunez",
+    repo: "check-my-bundlephobia",
+    number: 1,
     headers: {accept: "application/vnd.github.v3.diff"}
   });
