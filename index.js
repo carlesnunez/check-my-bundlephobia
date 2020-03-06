e87d9b9a3b2b92321d814b7d44f239097dd21a46
Carles Núñez Tomeo <carlesnunezt@gmail.com>
2020-03-06 22:39:58 +0100

    FEAT - Add table

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const fetch = require('node-fetch');
 const { exec } = require('child_process');
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({
+    auth: process.env.TOKEN
+});
+
+function getMarkDownTable(report) {
+    console.log(report)
+    let table = `
+## 😱 Check my bundlephobia - New/Modifies packages report:
+
+| name | gzip | size |
+| ----------- | ----------------------------------------------------------------------------- | --------------------------------------------------------------------------------- |
+`;
+    report.forEach((packageInfo, index) => {
+        console.log(packageInfo)
+      if (index === 0) {
+        table += `| ${packageInfo.name}  | ${packageInfo.gzip}         | ${packageInfo.size}         |
+`;
+      }
+    });
+
+    return table;
+  }
 
 exec(`git diff refs/remotes/origin/${process.env.GITHUB_BASE_REF} refs/remotes/origin/${process.env.GITHUB_HEAD_REF} package.json`, (err, out, e) => {
-    const [_, diff] = out.split('dependencies": ');
-    const packageList = diff.split("\n").filter(e => e.includes('+   '));
-console.log(out)
+    const diff = out.split("\n");
+    const stuffAdded = diff.filter(e => e.includes('+   ')).map(e => e.split(" ").join("").split("+").join("").split(",").join(""));
+    const packageList = stuffAdded.filter(name => {
+        const initIsQuote = name[0] === "\"" || name[0] === "'"
+        const endIsQuote = name[name.length - 1] === "\"" || name[name.length - 1] === "'"
+        const colonIndex = name.indexOf(":")
+        const quoteBeforeColon = name[colonIndex-1] === "\"" || name[colonIndex] === "'"
+        const quoteAfterColon = name[colonIndex+1] === "\"" || name[colonIndex] === "'"
+
+        return initIsQuote && endIsQuote && colonIndex && quoteAfterColon && quoteBeforeColon
+    })
     const changedPackages = packageList.map(name => {
         const noSpaces = name.split(" ").join("").split("+").join();
         const noBreaks = noSpaces.split("\n").join("")
@@ -26,8 +57,16 @@ console.log(out)
               sizes.push({name: l.name, gzip: l.gzip, size: l.size})
           }
         })));
-console.log(changedPackages)
-        Promise.all(requests).then((r) => {
-            console.log(sizes)
+
+        Promise.all(requests).then(() => {
+            const [owner, repositoryName] = process.env.GITHUB_REPOSITORY.split("/")
+            octokit.issues.createComment(
+                {
+                  owner,
+                  repo: repositoryName,
+                  issue_number: process.env.GITHUB_REF.split("refs/pull/")[1].split("/")[0],
+                  body: getMarkDownTable(sizes)
+                })
+
         })
 });
