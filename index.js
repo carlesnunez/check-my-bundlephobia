b5dafcd8ef2b40545bbdd75fcfe7fa332b63a41a
Carles Núñez Tomeo <carlesnunezt@gmail.com>
2020-03-06 01:14:42 +0100

    t

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const { Octokit } = require("@octokit/rest");
 const octokit = new Octokit();
 
-octokit.pullRequests.get({
+octokit.pulls.get({
     owner: "carlesnunez",
     repo: "check-my-bundlephobia",
-    number: 1,
-    headers: {accept: "application/vnd.github.v3.diff"}
-  });
+    pull_number: 2
+  }).then(e => console.log(e)).catch(console.log);
