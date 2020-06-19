f3869ece678d3e3c4fe8202018c7c349c2057747
Carles Núñez Tomeo <carlesnunezt@gmail.com>
2020-06-19 23:56:23 +0200

    l

--- src/utils.js
+++ src/utils.js
@@ -18,7 +18,7 @@ exports.getMarkDownTable = (sizesAdded, sizesRemoved) => {
 `;
   sizesAdded.forEach((packageInfo, index) => {
     const sizeRemoved = sizesRemoved.find(({name, package}) => name === packageInfo.name);
-    console.log(sizeRemoved.package, packageInfo.package)
+    sizeRemoved && console.log(sizeRemoved.package, packageInfo.package)
     if(!sizeRemoved || (sizeRemoved.package !== packageInfo.package)) {
       const gzipSize = (parseInt(packageInfo.gzip) / 1024).toFixed(1);
       const rawSize = (packageInfo.size / 1024).toFixed(1);
