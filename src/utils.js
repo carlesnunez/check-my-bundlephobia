809bff7900c36597325a7e67b373e5ec83814f62
Carles Núñez Tomeo <carlesnunezt@gmail.com>
2020-06-19 23:43:31 +0200

    a

--- src/utils.js
+++ src/utils.js
@@ -29,11 +29,14 @@ console.log('---->',sizeRemoved)
     const removedGzipSize = (parseInt(sizeRemoved.gzip) / 1024).toFixed(1);
     const removedRawSize = (sizeRemoved.size / 1024).toFixed(1)
     const removedIsBlockedMessage = packageInfo.gzip > core.getInput("threshold") ? "❌" : "✅"
-    table += `| Old | [${sizeRemoved.package}](https://bundlephobia.com/result?p=${sizeRemoved.package})  | ${removedGzipSize}kB         | ${removedRawSize}kB         | ${removedIsBlockedMessage}`;
+    table += `| Old | [${sizeRemoved.package}](https://bundlephobia.com/result?p=${sizeRemoved.package})  | ${removedGzipSize}kB         | ${removedRawSize}kB         | ${removedIsBlockedMessage}
+`;
     
     const gzipedDiff = (((parseInt(packageInfo.gzip) / 1024).toFixed(1)) - ((parseInt(sizeRemoved.gzip) / 1024).toFixed(1))).toFixed(1);
     const sizeDiff = (((parseInt(packageInfo.size) / 1024).toFixed(1)) - ((parseInt(sizeRemoved.size) / 1024).toFixed(1))).toFixed(1);
+   
     table += `| | | ${Math.sign(gzipedDiff) &&  gzipedDiff !== '0.0' ? '+' : ''}${gzipedDiff !== '0.0' ? gzipedDiff + 'kB' : ''}         | ${Math.sign(sizeDiff) && sizeDiff !== '0.0' ? '+' : ''}${sizeDiff !== '0.0' ? sizeDiff + 'kB' : ''}        | `;
+  
   }
   });
 
